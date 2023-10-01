475cac2858203dff5d6b517bfb8e40c6216dbcc5
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2023-10-01 00:21:24 -0500

    Boceto de función PARSEBNF

--- Taller2LP.rkt
+++ Taller2LP.rkt
@@ -17,3 +17,69 @@
 
 ;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
 ;; DESARROLLO
+
+;;Constructores
+
+(define (conjuncion)
+  'and)
+
+(define (disyuncion)
+  'or)
+
+(define or-expression
+  (lambda (a b c)
+    (list 'or-expression (a b c))))
+
+(define and-expression
+  (lambda (q w e)
+    (list 'and-expression (q w e))))
+
+;;Predicados
+
+(define or-expression?
+  (lambda (disyuncion)
+    (eq? 'or-expression (cadr disyuncion))))
+
+(define and-expression?
+  (lambda (conjuncion)
+    (eq? 'or-expression (cadr conjuncion))))
+
+;; Demostración
+
+(define or-expression->or
+  (lambda (sym)
+    (cadr sym)))
+
+(define or-expression->left
+  (lambda (sym)
+    (car sym)))
+
+(define or-expression->right
+  (lambda (sym)
+    (caddr sym)))
+
+(define and-expression->and
+  (lambda (sym)
+    (cadr sym)))
+
+(define and-expression->left
+  (lambda (sym)
+    (car sym)))
+
+(define and-expression->right
+  (lambda (sym)
+    (caddr sym)))
+
+;;Programación
+
+(define PARSEBNF
+  (lambda (parse)
+    (cond
+      [(or-expression? parse)
+       (or-expression->or parse)
+       (PARSEBNF (or-expression->left))
+       (PARSEBNF (or-expression->right))]
+      [(and-expression? parse)
+       (and-expression->and parse)
+       (PARSEBNF (and-expression->left))
+       (PARSEBNF (and-expression->right))])))
